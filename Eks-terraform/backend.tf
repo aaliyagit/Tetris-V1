3d1c56989f6168ea2fd8b549d1ed810170255ad0
aaliyagit <149709011+aaliyagit@users.noreply.github.com>
2023-12-24 13:57:18 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "atmdatastore" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "us-west-2"
   }
 }
