15118d388d4ce927ce9bd87c5862aab1f911f827
aaliyagit <149709011+aaliyagit@users.noreply.github.com>
2023-12-27 07:46:17 -0500

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-008fe2fc65df48dac"
   instance_type          = "t2.large"
-  key_name               = "All-keys"
+  key_name               = "virginia_keys"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
